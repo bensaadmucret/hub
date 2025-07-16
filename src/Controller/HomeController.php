bc937bdfaefa7fe42c11d4d071e2eeaf81da36be
Bensaad Mohammed <bensaadmohammed@MacBook-Pro-de-Bensaad.local>
2025-07-16 02:19:12 +0200

    Ajout des composants formulaire de contact

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    private const HOMEPAGE_SLUG = 'acceuil';
+
+    public function __construct(private PageRepository $pageRepository)
+    {
+    }
+
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        $page = $this->pageRepository->findOneBySlug(self::HOMEPAGE_SLUG);
+        if (!$page) {
+            throw $this->createNotFoundException('Page d\'accueil non trouvée');
+        }
+
+        return $this->render('page/show.html.twig', [
+            'page' => $page,
+        ]);
+    }
+}
